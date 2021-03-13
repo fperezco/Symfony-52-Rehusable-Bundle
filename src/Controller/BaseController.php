6e4b1b67f9b0350d5edf71e184bb486337322d67
jonytrifulca <jonytrifulca@hotmail.com>
2021-03-13 09:41:20 +0100

    add apache-pack, an example route, and some code

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    /**
+     * @Route("/base", name="base")
+     */
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
