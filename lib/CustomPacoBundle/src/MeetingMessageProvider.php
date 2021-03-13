3cbd5136771ec09d2170946207a894ea99ac8a12
jonytrifulca <jonytrifulca@hotmail.com>
2021-03-13 19:19:24 +0100

    add a new class to inject, before update services.yml

---
+++ lib/CustomPacoBundle/src/MeetingMessageProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Paco\CustomPacoBundle;
+
+
+class MeetingMessageProvider
+{
+    public function getWordList(): array
+    {
+        return [
+            'Hi adorable ',
+            'Hi active ',
+            'Hi admire ',
+            'Hi adventurous ',
+            'Hi agreeable ',
+        ];
+    }
+}
--- lib/CustomPacoBundle/src/PacoNumberGenerator.php
+++ lib/CustomPacoBundle/src/PacoNumberGenerator.php
@@ -6,13 +6,20 @@ class PacoNumberGenerator
 {
     private $baseNumber;
     private $topNumber;
+    private $meetingMessageProvider;
 
-    public function __construct($baseNumber = 0, $topNumber = 100){
+    public function __construct(MeetingMessageProvider $meetingMessageProvider,$baseNumber = 0, $topNumber = 100){
         $this->baseNumber = $baseNumber;
         $this->topNumber = $topNumber;
+        $this->meetingMessageProvider = $meetingMessageProvider;
     }
 
     public function getNumber(){
         return random_int($this->baseNumber, $this->topNumber);
+    }
+
+    private function getWordList(): array
+    {
+        return $this->meetingMessageProvider->getWordList();
     }
 }
